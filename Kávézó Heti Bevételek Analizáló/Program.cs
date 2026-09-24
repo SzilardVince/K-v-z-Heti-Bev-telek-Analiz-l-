87c8ad25fd6b6938783702d50236d70b7fec0fd8
SzilardVince <szilard.vince.2024d@telekimezotur.hu>
2026-09-24 12:57:03 +0200

    feladat 5

--- Kávézó Heti Bevételek Analizáló/Program.cs
+++ Kávézó Heti Bevételek Analizáló/Program.cs
@@ -1,5 +1,6 @@
 ﻿List<int> szl = new List<int>();
-Console.WriteLine("=== Kávézó Heti Bevételek Rögzítése ===");
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("=== kávézó Heti Bevételek Rögzítése ===");
 for (int i=0;i<5;i++)
 {
     Console.Write($"{i+1}. nap bevétele (Ft): ");
@@ -12,5 +13,21 @@ for (int i = 0; i < 5; i++)
     osszeg += szl[i];
 }
 double atlag = osszeg / 5.0;
+Console.WriteLine("========================================");
+Console.ForegroundColor = ConsoleColor.Green;
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"- {i}. nap: {szl[i]}Ft");
+}
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("========================================");
+Console.ForegroundColor = ConsoleColor.Magenta;
 Console.WriteLine($"Összesített bevétel: {osszeg} Ft");
 Console.WriteLine($"Napi átlagbevétel: {atlag:F2} Ft");
+string statusz;
+if (atlag >= 80000) statusz = "kiemelkedö hét!!!";
+else if (atlag >= 50000) statusz=  "átlagos hét!!!";
+else statusz =  "Gyenge hét, felülvizsgálat szükséges!!!";
+Console.WriteLine($"heti státusz: {statusz}");
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("========================================");
