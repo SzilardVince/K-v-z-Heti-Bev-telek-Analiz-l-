4de2710f2baa2a37e241962e0346d9e08fa45e2f
SzilardVince <szilard.vince.2024d@telekimezotur.hu>
2026-09-24 12:43:28 +0200

    feladat 3

--- Kávézó Heti Bevételek Analizáló/Program.cs
+++ Kávézó Heti Bevételek Analizáló/Program.cs
@@ -5,4 +5,12 @@ for (int i=0;i<5;i++)
     Console.Write($"{i+1}. nap bevétele (Ft): ");
     int napi = int.Parse(Console.ReadLine());
     szl.Add(napi);
-} 
+}
+int osszeg = 0;
+for (int i = 0; i < 5; i++)
+{
+    osszeg += szl[i];
+}
+double atlag = osszeg / 5.0;
+Console.WriteLine($"Összesített bevétel: {osszeg} Ft");
+Console.WriteLine($"Napi átlagbevétel: {atlag:F2} Ft");
