7508a37e7ee621be564f892c99796fe42a66b361
SzilardVince <szilard.vince.2024d@telekimezotur.hu>
2026-09-24 12:31:43 +0200

    feladat 2.

--- Kávézó Heti Bevételek Analizáló/Program.cs
+++ Kávézó Heti Bevételek Analizáló/Program.cs
@@ -1,2 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
- 
+﻿List<int> szl = new List<int>();
+Console.WriteLine("=== Kávézó Heti Bevételek Rögzítése ===");
+for (int i=0;i<5;i++)
+{
+    Console.Write($"{i+1}. nap bevétele (Ft): ");
+    int napi = int.Parse(Console.ReadLine());
+    szl.Add(napi);
+}
